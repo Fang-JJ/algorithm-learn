f48c4f4d6dfa50ac8b1593ae6846dd44074fce41
fangjiajie <fangjiajie@polyv.net>
2022-01-22 23:57:33 +0800

    并查集原理实现-补充注释

--- 01.一些基本算法的实现/01.union-find.js
+++ 01.一些基本算法的实现/01.union-find.js
@@ -2,13 +2,19 @@ class UF {
     constructor(n) {
         this.count = n; // 初始连同分量的数量
         this.parent = new Array(n); // 初始化的时候每个元素的父元素都是自己，这符合自反性
-        this.size = new Array(n); // 初始化'重量'数组
+        // 初始化'重量'数组，目的是进行平衡性优化，防止只是单纯的小节点连接到大节点造成节点分组不平衡
+        this.size = new Array(n);
         for (let i = 0; i < n; i++) {
             this.parent[i] = i;
-            this.size[i] = i;
+            this.size[i] = 1;
         }
     }
 
+    /**
+     * 连接两个节点
+     * @param p
+     * @param q
+     */
     union(p, q) {
         const rootP = this.find(p);
         const rootQ = this.find(q);
@@ -21,9 +27,15 @@ class UF {
             this.parent[rootP] = rootQ;
             this.size[rootQ] += this.size[rootP];
         }
+        // 连接之后连通分量减1
         this.count--;
     }
 
+    /**
+     * 进行路径压缩
+     * @param x
+     * @returns {*}
+     */
     find(x) {
         while (this.parent[x] !== x) {
             this.parent[x] = this.parent[this.parent[x]];
@@ -32,10 +44,20 @@ class UF {
         return x;
     }
 
+    /**
+     * 返回当前连通分量的数量
+     * @returns {*}
+     */
     countNum() {
         return this.count;
     }
 
+    /**
+     * 判断两个节点是否连通
+     * @param p
+     * @param q
+     * @returns {boolean}
+     */
     connected(p, q) {
         const rootQ = this.find(q);
         const rootP = this.find(p);
